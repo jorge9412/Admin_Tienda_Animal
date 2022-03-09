78b3aef25f506d25ed381580e3ff5104e15b8c13
jorge9412 <jorgeivancifuentescorrea@gmail.com>
2022-03-08 19:56:31 -0500

    errores

--- TiendaAnimal/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ TiendaAnimal/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -56,7 +56,7 @@ namespace TiendaAnimal {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TiendaAnimal;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TiendaAnimal;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
